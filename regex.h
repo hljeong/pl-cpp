500313c2617cb5461a73162ac12e492199c47f84
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2025-05-05 23:08:41 -0700

    sum type working... also regex lexer

---
+++ regex.h
@@ -0,0 +1,280 @@
+#include <memory>
+#include <regex>
+#include <stdexcept>
+
+#include "lib/cpp_utils/cpy/cpy.h"
+#include "lib/cpp_utils/fmt/fmt.h"
+#include "sum.h"
+
+// todo: split out impl
+using namespace cpy;
+using namespace fmt;
+using namespace sum;
+
+struct Pipe {};
+struct Asterisk {};
+struct LParenthesis {};
+struct RParenthesis {};
+
+struct Token : OneOf<Pipe, Asterisk, LParenthesis, RParenthesis, char> {};
+
+inline String repr(const Pipe &value) { return "'|'"; }
+inline String repr(const Asterisk &value) { return "'*'"; }
+inline String repr(const LParenthesis &value) { return "'('"; }
+inline String repr(const RParenthesis &value) { return "')'"; }
+
+using TokenStream = List<Token>;
+
+inline TokenStream lex(const String &source) {
+  const size_t n = source.size();
+  size_t i = 0;
+  TokenStream r;
+  while (i < n) {
+    while (i < n && std::isspace(source[i])) {
+      i++;
+    }
+
+    if (i >= n) {
+      break;
+    }
+
+    switch (source[i]) {
+    case '|':
+      r.push_back({Pipe{}});
+      break;
+    case '*':
+      r.push_back({Asterisk{}});
+      break;
+    case '(':
+      r.push_back({LParenthesis{}});
+      break;
+    case ')':
+      r.push_back({RParenthesis{}});
+      break;
+    default:
+      r.push_back({source[i]});
+      break;
+    }
+
+    i++;
+  }
+  return r;
+}
+
+struct Regex;
+struct UnionExpr;
+struct ConcatExpr;
+struct RepeatExpr;
+struct BaseExpr;
+struct Quantifier {};
+
+struct UnionExpr0;
+struct UnionExpr1;
+struct ConcatExpr0;
+struct ConcatExpr1;
+struct RepeatExpr0;
+struct RepeatExpr1;
+struct BaseExpr0;
+struct BaseExpr1;
+
+struct BaseExpr : OneOf<BaseExpr0, BaseExpr1> {};
+struct RepeatExpr : OneOf<RepeatExpr0, RepeatExpr1> {};
+struct ConcatExpr : OneOf<ConcatExpr0, ConcatExpr1> {};
+struct UnionExpr : OneOf<UnionExpr0, UnionExpr1> {};
+
+struct Regex {
+  UnionExpr expr;
+};
+
+struct UnionExpr0 {
+  ConcatExpr first;
+  UnionExpr rest;
+};
+
+struct UnionExpr1 {
+  ConcatExpr expr;
+};
+
+struct ConcatExpr0 {
+  RepeatExpr first;
+  ConcatExpr rest;
+};
+
+struct ConcatExpr1 {
+  RepeatExpr expr;
+};
+
+struct RepeatExpr0 {
+  BaseExpr expr;
+  Quantifier quantifier;
+};
+
+struct RepeatExpr1 {
+  BaseExpr expr;
+};
+
+struct BaseExpr0 {
+  Regex regex;
+};
+
+struct BaseExpr1 {
+  char c;
+};
+
+constexpr struct Error {
+} error;
+
+template <typename T> struct Result : OneOf<Error, T> {
+  using Base = OneOf<Error, T>;
+
+  Result(Error value) : Base{value} {}
+
+  Result(const T &value) : Base{value} {}
+
+  operator bool() const { return Base::template is<T>(); }
+
+  const T &operator*() const {
+    if (!*this) {
+      throw std::invalid_argument("cannot get result value from error");
+    } else {
+      return Base::template as<T>();
+    }
+  }
+
+  const T *operator->() const { return &Base::template as<T>(); }
+};
+
+/*
+ * Regex -> UnionExpr
+ * UnionExpr -> ConcatExpr "|" UnionExpr | ConcatExpr
+ * ConcatExpr -> RepeatExpr ConcatExpr | RepeatExpr
+ * RepeatExpr -> BaseExpr "*" | BaseExpr
+ * BaseExpr -> "(" Regex ")" | char
+ */
+
+class Parser {
+public:
+  Parser(const TokenStream &token_stream_) : token_stream(token_stream_) {}
+
+  Result<Regex> parse() {
+    auto regex = parse_regex();
+    if (idx != token_stream.size()) {
+      return error;
+    }
+    return regex;
+  }
+
+private:
+  const TokenStream &token_stream;
+  uint idx = 0;
+
+  Result<Regex> parse_regex();
+  Result<UnionExpr> parse_union_expr();
+  Result<ConcatExpr> parse_concat_expr();
+  Result<RepeatExpr> parse_repeat_expr();
+  Result<BaseExpr> parse_base_expr();
+
+  class Checkpoint {
+  public:
+    Checkpoint(uint &idx_) : idx(idx_), checkpoint(idx_) {}
+
+    void discard() { discarded = true; }
+
+    ~Checkpoint() {
+      if (!discarded) {
+        idx = checkpoint;
+      }
+    }
+
+  private:
+    uint &idx;
+    const uint checkpoint;
+    bool discarded = false;
+  };
+
+  inline Result<Token> consume() {
+    if (idx < token_stream.size()) {
+      return token_stream[idx++];
+    } else {
+      return error;
+    }
+  }
+
+  template <typename T> inline Result<T> expect() {
+    Checkpoint checkpoint(idx);
+    auto token = consume();
+    if (!token || !token->is<T>()) {
+      return error;
+    }
+    checkpoint.discard();
+    return token->as<T>();
+  }
+};
+
+inline Result<Regex> Parser::parse_regex() {
+  auto expr = parse_union_expr();
+  if (!expr) {
+    return error;
+  }
+  return Regex{*expr};
+  // return Regex{expr.as<UnionExpr>()};
+}
+
+inline Result<UnionExpr> Parser::parse_union_expr() {
+  auto first = parse_concat_expr();
+  if (!first) {
+    return error;
+  }
+
+  if (!expect<Pipe>()) {
+    return UnionExpr{UnionExpr1{*first}};
+  }
+
+  auto rest = parse_union_expr();
+  if (!rest) {
+    return error;
+  }
+
+  return UnionExpr{UnionExpr0{*first, *rest}};
+}
+
+inline Result<ConcatExpr> Parser::parse_concat_expr() {
+  auto first = parse_repeat_expr();
+  if (!first) {
+    return error;
+  }
+
+  auto rest = parse_concat_expr();
+  return rest ? ConcatExpr{ConcatExpr0{*first, *rest}}
+              : ConcatExpr{ConcatExpr1{*first}};
+}
+
+inline Result<RepeatExpr> Parser::parse_repeat_expr() {
+  auto expr = parse_base_expr();
+  if (!expr) {
+    return error;
+  }
+
+  return expect<Asterisk>() ? RepeatExpr{RepeatExpr0{*expr}}
+                            : RepeatExpr{RepeatExpr1{*expr}};
+}
+
+inline Result<BaseExpr> Parser::parse_base_expr() {
+  Checkpoint checkpoint{idx};
+
+  if (expect<LParenthesis>()) {
+    Result<Regex> regex = parse_regex();
+    if (!regex || !expect<RParenthesis>()) {
+      return error;
+    }
+    checkpoint.discard();
+    return BaseExpr{BaseExpr0{*regex}};
+  } else {
+    auto token = expect<char>();
+    if (!token) {
+      return error;
+    }
+    checkpoint.discard();
+    return BaseExpr{BaseExpr1{*token}};
+  }
+}
--- test.cc
+++ test.cc
@@ -6,72 +6,101 @@ using namespace cpy;
 using namespace fmt;
 using namespace nfa;
 
-int main() {
-  Nfa::Alphabet S{'a', 'b'};
-  Nfa::State q0, q1;
-  Nfa n1{{q0}, q0, S, {{q0, {{'a', {q0}}}}}, {q0}};
-  Nfa n2{{q1}, q1, S, {{q1, {{'b', {q1}}}}}, {q1}};
-  Nfa n3 = n1 + n2;
-  Nfa n4 = n1 | n2;
-  Nfa n5 = *(n1 + n2);
+#include "regex.h"
+// #include "sum.h"
 
-  List<String> strings{"", "a", "aa", "ab", "bb", "aabb", "ba", "baa"};
-  List<Pair<Nfa, List<Pair<String, bool>>>> tests;
-  tests.push_back({n1,
-                   {{"", true},
-                    {"a", true},
-                    {"aa", true},
-                    {"ab", false},
-                    {"bb", false},
-                    {"aabb", false},
-                    {"ba", false},
-                    {"baa", false}}});
-  tests.push_back({n2,
-                   {{"", true},
-                    {"a", false},
-                    {"aa", false},
-                    {"ab", false},
-                    {"bb", true},
-                    {"aabb", false},
-                    {"ba", false},
-                    {"baa", false}}});
-  tests.push_back({n3,
-                   {{"", true},
-                    {"a", true},
-                    {"aa", true},
-                    {"ab", true},
-                    {"bb", true},
-                    {"aabb", true},
-                    {"ba", false},
-                    {"baa", false}}});
-  tests.push_back({n4,
-                   {{"", true},
-                    {"a", true},
-                    {"aa", true},
-                    {"ab", false},
-                    {"bb", true},
-                    {"aabb", false},
-                    {"ba", false},
-                    {"baa", false}}});
-  tests.push_back({n5,
-                   {{"", true},
-                    {"a", true},
-                    {"aa", true},
-                    {"ab", true},
-                    {"bb", true},
-                    {"aabb", true},
-                    {"ba", true},
-                    {"baa", true}}});
-  for (const auto &[idx, nfa_tests] : enumerate(tests)) {
-    const auto &[nfa, tests_] = nfa_tests;
-    for (const auto &[s, expected] : tests_) {
-      bool result = nfa.accepts(s);
-      if (result != expected) {
-        print("n{}.accepts({}) => {} (expected: {})", idx + 1, repr(s), result,
-              expected);
-        printo(nfa);
-        return 1;
-      }
-    }
-  }
+int main() {
+  // Nfa::Alphabet S{'a', 'b'};
+  // Nfa::State q0, q1;
+  // Nfa n1{{q0}, q0, S, {{q0, {{'a', {q0}}}}}, {q0}};
+  // Nfa n2{{q1}, q1, S, {{q1, {{'b', {q1}}}}}, {q1}};
+  // Nfa n3 = n1 + n2;
+  // Nfa n4 = n1 | n2;
+  // Nfa n5 = *(n1 + n2);
+  //
+  // List<String> strings{"", "a", "aa", "ab", "bb", "aabb", "ba", "baa"};
+  // List<Pair<Nfa, List<Pair<String, bool>>>> tests;
+  // tests.push_back({n1,
+  //                  {{"", true},
+  //                   {"a", true},
+  //                   {"aa", true},
+  //                   {"ab", false},
+  //                   {"bb", false},
+  //                   {"aabb", false},
+  //                   {"ba", false},
+  //                   {"baa", false}}});
+  // tests.push_back({n2,
+  //                  {{"", true},
+  //                   {"a", false},
+  //                   {"aa", false},
+  //                   {"ab", false},
+  //                   {"bb", true},
+  //                   {"aabb", false},
+  //                   {"ba", false},
+  //                   {"baa", false}}});
+  // tests.push_back({n3,
+  //                  {{"", true},
+  //                   {"a", true},
+  //                   {"aa", true},
+  //                   {"ab", true},
+  //                   {"bb", true},
+  //                   {"aabb", true},
+  //                   {"ba", false},
+  //                   {"baa", false}}});
+  // tests.push_back({n4,
+  //                  {{"", true},
+  //                   {"a", true},
+  //                   {"aa", true},
+  //                   {"ab", false},
+  //                   {"bb", true},
+  //                   {"aabb", false},
+  //                   {"ba", false},
+  //                   {"baa", false}}});
+  // tests.push_back({n5,
+  //                  {{"", true},
+  //                   {"a", true},
+  //                   {"aa", true},
+  //                   {"ab", true},
+  //                   {"bb", true},
+  //                   {"aabb", true},
+  //                   {"ba", true},
+  //                   {"baa", true}}});
+  // for (const auto &[idx, nfa_tests] : enumerate(tests)) {
+  //   const auto &[nfa, tests_] = nfa_tests;
+  //   for (const auto &[s, expected] : tests_) {
+  //     bool result = nfa.accepts(s);
+  //     if (result != expected) {
+  //       print("n{}.accepts({}) => {} (expected: {})", idx + 1, repr(s),
+  //       result,
+  //             expected);
+  //       printo(nfa);
+  //       return 1;
+  //     }
+  //   }
+  // }
+
+  using namespace sum;
+  OneOf<int, char> x = 3;
+  OneOf<int, char> y = 'h';
+  printo(x.is<int>(), x.is<char>());
+  printo(y.is<int>(), y.is<char>());
+  printo(x.as<int>(), y.as<char>());
+
+  // struct B {
+  //   B(int) {}
+  // };
+  //
+  // struct D : B {};
+  //
+  // B b = 3;
+  // D d = {3};
+  //
+  // struct S : OneOf<int, char> {};
+  //
+  // S sx = {3};
+  // printo(sx.as<int>());
+
+  // printo(x, y);
+  // printo(y.OneOf<char>::is_T);
+  printo(lex("(a*b*)"));
 }
