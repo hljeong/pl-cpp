c4667dcfad876b88e917c445e3acdb8936873c52
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2025-05-08 23:34:17 -0700

    package whatever

--- sum2.h
+++ sum2.h
@@ -3,12 +3,6 @@
 #include <memory>
 #include <type_traits>
 
-// #include "lib/cpp_utils/cpy/cpy.h"
-// #include "lib/cpp_utils/fmt/fmt.h"
-//
-// using namespace cpy;
-// using namespace fmt;
-
 #include "lib/cpp_utils/hlj/hlj.h"
 
 using namespace hlj;
@@ -27,265 +21,6 @@ template <typename T, typename U, typename... Ts> struct index_t<T, U, Ts...> {
 
 template <typename T, typename... Ts>
 static constexpr size_t index = index_t<T, Ts...>::value;
-
-// template <typename F, typename... Ts, size_t... Is>
-// auto map_(F f, const Tuple<Ts...> &t, std::index_sequence<Is...>) {
-//   return std::make_tuple(
-//       f(std::get<Is>(std::forward<const Tuple<Ts...> &>(t)))...);
-// }
-//
-// template <typename F, typename... Ts> auto map(F f, const Tuple<Ts...> &t) {
-//   return map_(std::forward<F>(f), std::forward<const Tuple<Ts...> &>(t),
-//               std::make_index_sequence<sizeof...(Ts)>());
-// }
-//
-// template <typename T> std::unique_ptr<T> copy(const std::unique_ptr<T> &p) {
-//   return p ? std::make_unique<T>(*p) : nullptr;
-// }
-
-// static constexpr bool debug_print = true;
-//
-// template <typename T> void *copy(const void *value) {
-//   void *p = new T{*reinterpret_cast<const T *>(value)};
-//   if (debug_print) {
-//     print("copied to {} {} @ {}", type_name<T>(),
-//           repr(*reinterpret_cast<T *>(p)), long(p));
-//   }
-//   return p;
-// }
-//
-// template <typename T> void *move(const void *value) {
-//   void *p = new T{std::move(*reinterpret_cast<const T *>(value))};
-//   if (debug_print) {
-//     print("moved to {} {} @ {}", type_name<T>(),
-//           repr(*reinterpret_cast<T *>(p)), long(p));
-//   }
-//   return p;
-// }
-//
-// template <typename T> void destroy(void *value) {
-//   if (debug_print) {
-//     print("destroying {} {} @ {}", type_name<T>(),
-//           repr(*reinterpret_cast<T *>(value)), long(value));
-//   }
-//   delete reinterpret_cast<T *>(value);
-//   value = nullptr;
-//   if (debug_print) {
-//     print("done destroying");
-//   }
-// }
-//
-// template <typename... Ts> class OneOf {
-// public:
-//   template <typename T>
-//   OneOf(const T &value)
-//       : copy{sum::copy<T>}, move{sum::move<T>}, destroy{sum::destroy<T>},
-//         repr_{[](void *p) {
-//           using fmt::repr;
-//           return repr(*reinterpret_cast<T *>(p));
-//         }},
-//         which{index<T, Ts...>}, store{copy(&value)} {
-//     if (debug_print) {
-//       print("value construct");
-//     }
-//   }
-//
-//   OneOf(const OneOf &value)
-//       : copy{value.copy}, move{value.move}, destroy{value.destroy},
-//         repr_{value.repr_}, which{value.which}, store{copy(value.store)} {
-//     if (debug_print) {
-//       print("copy construct");
-//     }
-//   }
-//
-//   template <typename T> OneOf &operator=(const T &other) {
-//     if (debug_print) {
-//       print("value copy assign");
-//     }
-//     destroy(store);
-//
-//     copy = sum::copy<T>;
-//     move = sum::move<T>;
-//     destroy = sum::destroy<T>;
-//     repr_ = [](void *p) {
-//       using fmt::repr;
-//       return repr(*reinterpret_cast<T *>(p));
-//     };
-//     which = index<T, Ts...>;
-//     store = copy(other.store);
-//   }
-//
-//   OneOf &operator=(const OneOf &other) {
-//     if (debug_print) {
-//       print("copy assign");
-//     }
-//     destroy(store);
-//
-//     copy = other.copy;
-//     move = other.move;
-//     destroy = other.destroy;
-//     repr_ = other.repr_;
-//     which = other.which;
-//     store = copy(other.store);
-//
-//     return *this;
-//   }
-//
-//   OneOf(OneOf &&value)
-//       : copy{value.copy}, move{value.move}, destroy{value.destroy},
-//         repr_{value.repr_}, which{value.which}, store{move(value.store)} {
-//     if (debug_print) {
-//       print("move construct");
-//     }
-//     value.destroy(value.store);
-//     value.which = N;
-//     value.destroy = [](auto) {};
-//     value.move = [](auto) { return nullptr; };
-//     value.copy = [](auto) { return nullptr; };
-//   }
-//
-//   template <typename T> OneOf &operator=(T &&other) {
-//     if (debug_print) {
-//       print("value move assign");
-//     }
-//     destroy(store);
-//
-//     copy = sum::copy<T>;
-//     move = sum::move<T>;
-//     destroy = sum::destroy<T>;
-//     repr_ = [](void *p) {
-//       using fmt::repr;
-//       return repr(*reinterpret_cast<T *>(p));
-//     };
-//     which = index<T, Ts...>;
-//     store = move(other.store);
-//   }
-//
-//   OneOf &operator=(OneOf &&other) {
-//     if (debug_print) {
-//       print("move assign");
-//     }
-//     destroy(store);
-//
-//     copy = other.copy;
-//     move = other.move;
-//     destroy = other.destroy;
-//     repr_ = other.repr_;
-//     which = other.which;
-//     store = move(other.store);
-//
-//     other.destroy(other.store);
-//     other.which = N;
-//     other.destroy = [](auto) {};
-//     other.move = [](auto) { return nullptr; };
-//     other.copy = [](auto) { return nullptr; };
-//
-//     return *this;
-//   }
-//
-//   ~OneOf() {
-//     if (debug_print) {
-//       print("destructor ({})", which == N ? "nothing" : ::repr(which));
-//     }
-//     destroy(store);
-//   }
-//
-//   template <typename T> bool is() const { return which == index<T, Ts...>; }
-//
-//   template <typename T> const T &as() const {
-//     if (!is<T>()) {
-//       throw std::invalid_argument(
-//           format("cannot as<{}>()", type_name<T>(), type_name<T>()));
-//     } else {
-//       return *reinterpret_cast<const T *>(store);
-//     }
-//   }
-//
-//   template <typename T> bool operator==(const T &other) const {
-//     return (which == index<T, Ts...>) && (as<T>() == other);
-//   }
-//
-//   template <typename T> bool operator<(const T &other) const {
-//     return (which < index<T, Ts...>) ||
-//            ((which == index<T, Ts...>) && (as<T>() < other));
-//   }
-//
-//   String repr() const { return repr_(store); }
-//
-// private:
-//   static constexpr size_t N = sizeof...(Ts);
-//   // using Store = std::aligned_union_t<N, Ts...>;
-//
-//   std::function<void *(const void *)> copy;
-//   std::function<void *(const void *)> move;
-//   std::function<void(void *)> destroy;
-//   std::function<String(void *)> repr_;
-//
-//   // const static std::array<std::function<void *(const void *)>, N> copy = {
-//   //     sum::copy<Ts>...,
-//   // };
-//   //
-//   // const static std::array<std::function<void *(const void *)>, N> move = {
-//   //     sum::move<Ts>...,
-//   // };
-//   //
-//   // const static std::array<std::function<void(const void *)>, N> destroy =
-//   {
-//   //     sum::destroy<Ts>...,
-//   // };
-//
-//   // const std::array<std::function<void(const void *)>, N + 1> copy = {
-//   //     [this](const void *value) {
-//   //       print("start copying");
-//   //       // print("copying {} {}", type_name<Ts>(),
-//   //       //       repr(*reinterpret_cast<const Ts *>(value)));
-//   //       store = new Ts{*reinterpret_cast<const Ts *>(value)};
-//   //       // print("result: {}", repr(*reinterpret_cast<const Ts
-//   //       // *>(store)));
-//   //       print("done copying");
-//   //     }...,
-//   //     [](auto) { print("copying nothing"); }};
-//   //
-//   // const std::array<std::function<void(const void *)>, N + 1> move = {
-//   //     [this](const void *value) {
-//   //       print("moving {}", type_name<Ts>());
-//   //       store = new Ts{std::move(*reinterpret_cast<const Ts *>(value))};
-//   //       print("result: {}", repr(*reinterpret_cast<const Ts *>(store)));
-//   //     }...,
-//   //     [](auto) { print("moving nothing"); }};
-//   //
-//   // const std::array<std::function<void()>, N + 1> destroy = {
-//   //     [this]() {
-//   //       print("destroying {}", type_name<Ts>);
-//   //       delete reinterpret_cast<Ts *>(store);
-//   //       store = nullptr;
-//   //     }...,
-//   //     []() { print("destroying nothing"); }};
-//
-//   size_t which = N;
-//   void *store = nullptr;
-//   // Store store;
-//   //
-//   // const std::vector<std::function<void(const void *)>> copy = {
-//   //     [this](const void *value) {
-//   //       new (&store) Ts{*reinterpret_cast<const Ts *>(value)};
-//   //     }...,
-//   //     [](auto) {}};
-//   //
-//   // const std::vector<std::function<void(const void *)>> move = {
-//   //     [this](const void *value) {
-//   //       *reinterpret_cast<Ts *>(&store) =
-//   //           std::move(*reinterpret_cast<const Ts *>(value));
-//   //     }...,
-//   //     [](auto) {}};
-//   //
-//   // const std::vector<std::function<void()>> destroy = {
-//   //     [this]() { reinterpret_cast<Ts *>(&store)->~Ts(); }..., []() {}};
-// };
-
-// template <typename... Ts> String repr(const OneOf<Ts...> &value) {
-//   return value.repr();
-// }
 
 using ::repr;
 
@@ -312,44 +47,85 @@ struct reprable_t<T, std::void_t<decltype(repr(std::declval<T>()))>>
 
 template <typename T> constexpr bool reprable = reprable_t<T, void>::value;
 
-struct B {
-  virtual ~B() = 0;
-  virtual UPtr<B> copy() const = 0;
-  virtual const String &get_type_name() const = 0;
-  virtual String get_repr() const = 0;
-};
+// struct B {
+//   virtual ~B() = 0;
+//   virtual UPtr<B> copy() const = 0;
+//   virtual const String &get_type_name() const = 0;
+//   virtual String get_repr() const = 0;
+// };
+//
+// inline B::~B() {}
+//
+// template <typename T> struct D : B {
+//   const T value;
+//   static const String type_name;
+//
+//   D(const T &value_) : value{value_} {}
+//   ~D() override = default;
+//
+//   UPtr<B> copy() const final { return make_unique<D<T>>(value); }
+//
+//   const String &get_type_name() const final { return type_name; }
+//
+//   String get_repr() const final {
+//     if constexpr (reprable<T>) {
+//       return repr(value);
+//     } else {
+//       return format("<unreprable {}>", type_name);
+//     }
+//   }
+// };
+//
+// template <typename T> inline const String D<T>::type_name = ::type_name<T>;
 
-inline B::~B() {}
+// struct Whatever {
+//   UPtr<B> store;
+//
+//   Whatever() : store{nullptr} {}
+//
+//   template <typename T>
+//   Whatever(const T &value) : store{make_unique<D<std::decay_t<T>>>(value)} {}
+//
+//   Whatever(const Whatever &other)
+//       : store{other.store ? other.store->copy() : nullptr} {}
+//
+//   Whatever &operator=(const Whatever &other) {
+//     store = other.store ? other.store->copy() : nullptr;
+//     return *this;
+//   }
+//
+//   Whatever(Whatever &&other) : store{std::move(other.store)} {}
+//
+//   Whatever &operator=(Whatever &&other) {
+//     store = std::move(other.store);
+//     return *this;
+//   }
+//
+//   template <typename T> const T &as() const {
+//     return reinterpret_cast<D<T> *>(store.get())->value;
+//   }
+//
+//   String repr() const {
+//     return store ? format("{} {}", store->get_type_name(), store->get_repr())
+//                  : "nothing";
+//   }
+// };
 
-template <typename T> struct D : B {
-  const T value;
-  static const String type_name;
-
-  D(const T &value_) : value{value_} {}
-  ~D() override = default;
-
-  UPtr<B> copy() const final { return make_unique<D<T>>(value); }
-
-  const String &get_type_name() const final { return type_name; }
-
-  String get_repr() const final {
-    if constexpr (reprable<T>) {
-      return repr(value);
-    } else {
-      return format("<unreprable {}>", type_name);
-    }
+template <typename T> String always_repr(const T &value) {
+  if constexpr (reprable<T>) {
+    return repr(value);
+  } else {
+    return format("<unreprable {}>", type_name<T>);
   }
-};
+}
 
-template <typename T> inline const String D<T>::type_name = ::type_name<T>;
-
-struct Whatever {
-  UPtr<B> store;
-
+class Whatever {
+public:
   Whatever() : store{nullptr} {}
 
   template <typename T>
-  Whatever(const T &value) : store{make_unique<D<std::decay_t<T>>>(value)} {}
+  Whatever(const T &value)
+      : store{make_unique<Concrete<std::decay_t<T>>>(value)} {}
 
   Whatever(const Whatever &other)
       : store{other.store ? other.store->copy() : nullptr} {}
@@ -367,20 +143,43 @@ struct Whatever {
   }
 
   template <typename T> const T &as() const {
-    return reinterpret_cast<D<T> *>(store.get())->value;
+    return reinterpret_cast<Concrete<T> *>(store.get())->value;
   }
 
   String repr() const {
-    return store ? format("{} {}", store->get_type_name(), store->get_repr())
+    return store ? format("{} {}", store->get_type_name(), store->repr())
                  : "nothing";
   }
+
+private:
+  struct Store {
+    virtual ~Store() = 0;
+    virtual UPtr<Store> copy() const = 0;
+    virtual const String &get_type_name() const = 0;
+    virtual String repr() const = 0;
+  };
+
+  template <typename T> struct Concrete : Store {
+    const T value;
+    static const String type_name;
+
+    Concrete(const T &value_) : value{value_} {}
+    ~Concrete() override = default;
+
+    UPtr<Store> copy() const final { return make_unique<Concrete<T>>(value); }
+
+    const String &get_type_name() const final { return type_name; }
+
+    String repr() const final { return always_repr(value); }
+  };
+
+  UPtr<Store> store;
 };
 
-// inline String repr(const Whatever &value) {
-//   return value.store ? format("{} {}", value.store->get_type_name(),
-//                               value.store->get_repr())
-//                      : "nothing";
-// }
+inline Whatever::Store::~Store() {};
+
+template <typename T>
+inline const String Whatever::Concrete<T>::type_name = ::type_name<T>;
 
 template <typename... Ts> struct OneOf {
   Whatever store;
